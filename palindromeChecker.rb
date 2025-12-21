e5c97f4069ee7c0ca9018d070064f6db332000e4
DevManoj96 <meghwalmanoj9969@gmail.com>
2025-12-21 23:03:25 +0530

    PalindromeChecker

---
+++ palindromeChecker.rb
@@ -0,0 +1,34 @@
+
+# Palindrome Checker
+
+def palindromeChecker(text)
+  lst = []
+
+  for i in 0...text.length do 
+    lst.push(text[i])
+  end
+
+  rev_lst = lst.reduce([]) { |memo, el| memo.unshift(el) }
+  tmp = ""
+
+  rev_lst.each do |i|
+    tmp += i
+  end
+
+  if text.downcase == tmp.downcase
+    return true
+  else
+    return false
+  end
+end
+
+print "Text: "
+input = gets.chomp 
+
+a = palindromeChecker(input)
+if a
+  print "#{input}: It is a palindrome.\n"
+else
+  print "#{input}: It is not a palindrome.\n"
+end
+
